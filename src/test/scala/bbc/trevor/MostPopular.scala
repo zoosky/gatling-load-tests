62cfec5d7d2ef58c0edd552d214aff1f9933b8f3
AidyLewis <adrian_lewis@fastmail.fm>
2014-12-02 13:14:54 +0000

    most popular

---
+++ src/test/scala/bbc/trevor/MostPopular.scala
@@ -0,0 +1,27 @@
+package bbc.trevor
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class MostPopular extends Simulation {
+   
+    val httpProtocol = http
+        .baseURL("http://newsapps-trevor-producer.int.cloud.bbc.co.uk")
+
+    val scn = scenario("Access Most Popular")
+        .exec(http("Most Popular")
+        .get("/content/most_popular/news")
+        .check(status.is(200)))
+        .exec(http("Trending")
+        .get("/content/trending/events")
+        .check(status.is(200))
+    ) 
+
+    setUp(scn.inject(
+        rampUsersPerSec(10) to(250) during(2 minutes),
+        constantUsersPerSec(250) during(18 minutes)
+    ).protocols(httpProtocol))
+
+}
