c3939e67bbee4dd439aaa7a39db73219ba4f3d7f
AidyLewis <adrian_lewis@fastmail.fm>
2014-12-02 13:30:47 +0000

    resolver

---
+++ src/test/scala/bbc/trevor/Resolver.scala
@@ -0,0 +1,27 @@
+package bbc.trevor
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class Resolver extends Simulation {
+
+    val httpProtocol = http
+        .baseURL("http://newsapps-trevor-producer.int.cloud.bbc.co.uk")
+
+    val resolver = csv("trevor/resolver.csv").circular
+
+    val scn = scenario("Resolver")
+        .feed(resolver)
+        .exec(http("Resolver")
+        .get("${content}")
+        .check(status.is(200))
+    ) 
+
+    setUp(scn.inject(
+        rampUsersPerSec(10) to(250) during(2 minutes),
+        constantUsersPerSec(250) during(18 minutes)
+    ).protocols(httpProtocol))
+
+}
