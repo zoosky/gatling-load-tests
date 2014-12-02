c27cc126d267e33ec07c48b907c95afff3a865e0
AidyLewis <adrian_lewis@fastmail.fm>
2014-12-02 13:50:49 +0000

    producer cps event renderer

---
+++ src/test/scala/bbc/trevor/ProducerCpsEventRenderer.scala
@@ -0,0 +1,27 @@
+package bbc.trevor
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class ProducerCpsEventRenderer extends Simulation {
+
+    val httpProtocol = http
+        .baseURL("https://newsapps-trevor-dev-utils.int.cloud.bbc.co.uk")
+
+    val renderer = csv("trevor/producer-cps-event-renderer.csv").circular
+
+    val scn = scenario("Producer CPS Event Renderer")
+        .feed(renderer)
+        .exec(http("Producer CPS Event Renderer")
+        .get("${content}")
+        .check(status.is(200))
+    ) 
+
+    setUp(scn.inject(
+        rampUsersPerSec(10) to(250) during(2 minutes),
+        constantUsersPerSec(250) during(18 minutes)
+    ).protocols(httpProtocol))
+
+}
