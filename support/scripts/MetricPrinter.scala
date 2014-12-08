ee4b7c5fa487377a9a85cc40bf79a18042d833d5
AidyLewis <adrian_lewis@fastmail.fm>
2014-12-08 17:33:16 +0000

    simple date format

--- support/scripts/MetricPrinter.scala
+++ support/scripts/MetricPrinter.scala
@@ -1,11 +1,20 @@
+import java.util.Date
+import java.text.SimpleDateFormat
+
+  
 object MetricPrinter {
+  
+  def dateFormat(unixTime: Long) = {
+    val simpleDateFormat = new SimpleDateFormat("dd-M-yyyy HH:mm:ss")
+    val now = new Date(unixTime * 1000L)
+    simpleDateFormat.format(now)
+  }
 
   def main(args: Array[String]) {
     for { 
       ln <- io.Source.stdin.getLines 
       if ln.contains("allRequests.all.count")
         countList = ("""(\d+)""".r findAllIn ln).toList
-    } println("Time: " + countList(1) + " RPS: " + countList(0))
-    
+    } println("Time: " + dateFormat(countList(1).toLong) + ", RPS: " + countList(0))
   }
 }
