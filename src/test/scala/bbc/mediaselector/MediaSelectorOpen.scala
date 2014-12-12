cef5e3efcda3dbc4a9811b02c543a79bb4277533
AidyLewis <adrian_lewis@fastmail.fm>
2014-12-12 13:24:59 +0000

    Separate open test

--- src/test/scala/bbc/mediaselector/MediaSelectorOpen.scala
+++ src/test/scala/bbc/mediaselector/MediaSelectorOpen.scala
@@ -5,25 +5,20 @@ import scala.concurrent.duration._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class MediaSelector extends Simulation {
+class MediaSelectorOpen extends Simulation {
 
     val httpProtocol = http
         .header("X-IP-Address", "")
 
     val open = csv("media-selector/open.csv").circular
-    val secure = csv("media-selector/secure.csv").circular
 
     val scn = scenario("media-selector")
         .feed(open)
-        .feed(secure)
         .exec(http("open")
         .get("http://open.stage.cwwtf.bbc.co.uk${openUrl}")
         .check(status.is(200)))
-        .exec(http("secure")
-        .get("https://ipsecure.stage.bbc.co.uk${secureUrl}")
-        .check(status.is(200)))
 
     setUp(scn.inject(
-        rampUsersPerSec(10) to(750) during(20 minutes) 
+        rampUsersPerSec(10) to(250) during(10 minutes) 
     ).protocols(httpProtocol))
 }
