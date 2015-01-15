d7006de65c63e22272b9bca4e5cd1a17bfb3a89d
adrianlewis <adrian_lewis@fastmail.fm>
2015-01-15 14:31:56 +0000

    new ramp-up

--- src/test/scala/bbc/trevor/Resolver.scala
+++ src/test/scala/bbc/trevor/Resolver.scala
@@ -20,8 +20,7 @@ class Resolver extends Simulation {
     ) 
 
     setUp(scn.inject(
-        rampUsersPerSec(10) to(250) during(2 minutes),
-        constantUsersPerSec(250) during(18 minutes)
+        rampUsersPerSec(10) to(400) during(10 minutes)
     ).protocols(httpProtocol))
 
 }
