6fdd5220e933897645bc83f9204f4a700b81ee58
AdrianLewis <adrian_lewis@fastmail.fm>
2015-02-02 13:50:05 +0000

    things to do api via mobile site

--- src/test/scala/bbc/thingstodo/ThingsToDo.scala
+++ src/test/scala/bbc/thingstodo/ThingsToDo.scala
@@ -8,24 +8,25 @@ import io.gatling.http.Predef._
 class ThingsToDo extends Simulation { 
 
     val httpProtocol = http
-        .baseURL("https://api.stage.bbc.co.uk")
-        .acceptHeader("""application/ld+json""")
+        .baseURL("http://m.bbc.co.uk")
+        .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
+        .acceptHeader("""text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""")
         .acceptEncodingHeader("""gzip, deflate""")
+        .acceptLanguageHeader("""en-gb,en;q=0.5""")
         .connection("""keep-alive""")
+        .userAgentHeader("""Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30""")
 
-    val sportFeeder = csv("thingstodo/linked-data-sport-api.txt").circular
+    val thingsToDoFeeder = csv("thingstodo/[waiting for URLs]").circular
 
-    val scn = scenario("LDP Sport")
-        .feed(sportFeeder)
-        .exec(http("LDP Sport")
+    val scn = scenario("Things To Do")
+        .feed(thingsToDoFeeder)
+        .exec(http("things to do")
         .get("${sport}")
         .check(status.is(200)))
       
     setUp(scn.inject(
-        rampUsersPerSec(10) to(160) during(5 minutes),
-        constantUsersPerSec(160) during(10 minutes),
-        rampUsersPerSec(160) to(500) during(5 minutes),
-        constantUsersPerSec(500) during(5 minutes)
+        rampUsersPerSec(10) to(80) during(5 minutes),
+        constantUsersPerSec(80) during(15 minutes)
     ).protocols(httpProtocol))
 }
 
