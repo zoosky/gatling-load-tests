8bc4eebc68001367a3b8e46a37d6f1f7a6a1dea3
AdrianLewis <adrian_lewis@fastmail.fm>
2015-02-03 11:39:50 +0000

    new things to do script

---
+++ src/test/scala/bbc/sport/ThingsToDo.scala
@@ -0,0 +1,54 @@
+package bbc.sport
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class ThingsToDo extends Simulation { 
+    val httpProtocol = http
+        .baseURLs("http://m.bbc.co.uk", "http://m.bbc.com")
+        .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
+        .acceptHeader("""text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""")
+        .acceptEncodingHeader("""gzip, deflate""")
+        .acceptLanguageHeader("""en-gb,en;q=0.5""")
+        .connection("""keep-alive""")
+        .userAgentHeader("""Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30""")
+
+    val cricketFeeder = csv("sport/cricket-teams.csv").circular
+    val footballFeeder = csv("sport/football-teams.csv").circular 
+    val indexFeeder = csv("sport/indexes.csv").circular 
+    val rugbyLeagueFeeder = csv("sport/rugby-league-teams.csv").circular 
+    val rugbyUnionFeeder = csv("sport/rugby-union-teams.csv").circular 
+
+    val scn = scenario("Things To Do")
+        .feed(cricketFeeder)
+        .exec(http("Cricket")
+        .get("${cricketTeam}")
+        .check(status.is(200)))
+        
+        .feed(footballFeeder)
+        .exec(http("Football")
+        .get("${footballTeam}")
+        .check(status.is(200)))
+        
+        .feed(indexFeeder)
+        .exec(http("Indexes")
+        .get("${index}")
+        .check(status.is(200)))
+        
+        .feed(rugbyLeagueFeeder)
+        .exec(http("Rugby League")
+        .get("${rugbyLeagueTeam}")
+        .check(status.is(200)))
+
+        .feed(rugbyUnionFeeder)
+        .exec(http("Rugby Union")
+        .get("${rugbyUnionTeam}")
+        .check(status.is(200)))
+      
+    setUp(scn.inject(
+        rampUsersPerSec(10) to(80) during(5 minutes),
+        constantUsersPerSec(80) during(15 minutes)
+    ).protocols(httpProtocol))
+}
