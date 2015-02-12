2092e5f09623eeca30670731b7da82878514a7ef
PoornimaGunisetti <poornima.gunisetti@bbc.co.uk>
2015-02-12 10:19:24 +0000

    validate worldwide travel test

--- src/test/scala/bbc/worldwidetravel/WorldwideTravel.scala
+++ src/test/scala/bbc/worldwidetravel/WorldwideTravel.scala
@@ -1,74 +1,4 @@
-<<<<<<< HEAD
-///*
-///*
-//package bbc.worldwidetravel
-//
-//import scala.concurrent.duration._
-//
-//import io.gatling.core.Predef._
-//import io.gatling.http.Predef._
-//
-//
-//class WorldwideTravel extends Simulation {
-//
-//  val httpProtocol = http
-//    .baseURL("http://www.stage.bbc.com")
-//    .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
-//    .acceptHeader("""text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8""")
-//    .acceptEncodingHeader("""gzip, deflate""")
-//    .acceptLanguageHeader("""en-gb,en;q=0.5""")
-//    .connection("""keep-alive""")
-//    .userAgentHeader("""Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0""")
-//
-//  val travel_story_feed = csv("worldwide-travel/travel_story.csv").circular
-//  val travel_tagpage_feed = csv("worldwide-travel/travel_tagpage.csv").circular
-//
-//
-//  val scn = scenario("Worldwide Travel")
-//    .feed(travel_story_feed)
-//    .feed(travel_tagpage_feed)
-//
-//    .randomSwitch(
-//      25d -> exec(http("travel story")
-//        .get("${travel_story_urls")
-//        .check(status.is(200)))
-//
-//
-//
-//  25d -> group("HomePage") {
-//    exec(http("travel homepage")
-//      .get("/travel")
-//      .check(status.is(200)))
-//
-//  }
-//
-//  25d -> group("Tag Page") {
-//    exec(http("travel tag pages")
-//      .get("${travel_tagpage_urls}")
-//      .check(status.is(200)))
-//
-//  }
-//
-//  25d -> group("Destinations Index Page") {
-//    exec(http("destinations index page")
-//      .get("/travel/destinations")
-//      .check(status.is(200)))
-//
-//  }
-//
-//  )
-//
-//
-//  setUp(scn.inject(
-//    rampUsersPerSec(4) to(8) during(2 minutes),
-//    constantUsersPerSec(8) during(8 minutes),
-//    rampUsersPerSec(8) to(38) during(2 minutes),
-//    constantUsersPerSec(38) during(3 minutes)
-//  ).protocols(httpProtocol))
-//}
-//*/
-//*/
-=======
+
 package bbc.worldwidetravel
 
 import scala.concurrent.duration._
@@ -88,19 +18,50 @@ class WorldwideTravel extends Simulation {
     .connection("""keep-alive""")
     .userAgentHeader("""Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0""")
 
-  val travelUrl = csv("worldwide-travel/worldwidetravelurls.csv").circular
+  val travel_story_feed = csv("worldwide-travel/travel_story.csv").circular
+  val travel_tagpage_feed = csv("worldwide-travel/travel_tagpage.csv").circular
+
 
   val scn = scenario("Worldwide Travel")
-    .feed(travelUrl)
-    .exec(http("Travel v2")
-    .get("${travelurls}")
-    .check(status.is(200)))
+    .feed(travel_story_feed)
+    .feed(travel_tagpage_feed)
+
+    .randomSwitch(
+      25d -> exec(http("travel story")
+        .get("${travel_story_urls")
+        .check(status.is(200)))
+
+
+
+  25d -> group("HomePage") {
+    exec(http("travel homepage")
+      .get("/travel")
+      .check(status.is(200)))
+
+  }
+
+  25d -> group("Tag Page") {
+    exec(http("travel tag pages")
+      .get("${travel_tagpage_urls}")
+      .check(status.is(200)))
+
+  }
+
+  25d -> group("Destinations Index Page") {
+    exec(http("destinations index page")
+      .get("/travel/destinations")
+      .check(status.is(200)))
+
+  }
+
+  )
+
 
   setUp(scn.inject(
-    rampUsersPerSec(10) to(30) during(2 minutes),
-    constantUsersPerSec(30) during(8 minutes),
-    rampUsersPerSec(30) to(150) during(2 minutes),
-    constantUsersPerSec(150) during(3 minutes)
+    rampUsersPerSec(4) to(8) during(2 minutes),
+    constantUsersPerSec(8) during(8 minutes),
+    rampUsersPerSec(8) to(38) during(2 minutes),
+    constantUsersPerSec(38) during(3 minutes)
   ).protocols(httpProtocol))
 }
->>>>>>> fc8622cc2ace37cf8e0b827eedaae0831e6d9f1e
+
