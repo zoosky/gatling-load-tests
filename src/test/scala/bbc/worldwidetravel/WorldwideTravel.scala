c2d6e34fcc533d7e3c58c9b08359dfe68c9c728b
AdrianLewis <adrian_lewis@fastmail.fm>
2015-02-12 10:32:58 +0000

    first cut of worldwide test

--- src/test/scala/bbc/worldwidetravel/WorldwideTravel.scala
+++ src/test/scala/bbc/worldwidetravel/WorldwideTravel.scala
@@ -1,11 +1,9 @@
-
 package bbc.worldwidetravel
 
 import scala.concurrent.duration._
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-
 
 class WorldwideTravel extends Simulation {
 
@@ -18,44 +16,27 @@ class WorldwideTravel extends Simulation {
     .connection("""keep-alive""")
     .userAgentHeader("""Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:31.0) Gecko/20100101 Firefox/31.0""")
 
-  val travel_story_feed = csv("worldwide-travel/travel_story.csv").circular
-  val travel_tagpage_feed = csv("worldwide-travel/travel_tagpage.csv").circular
-
+  val travelStoryFeed = csv("worldwide-travel/travel_story.csv").circular
+  val travelTagPageFeed = csv("worldwide-travel/travel_tagpage.csv").circular
 
   val scn = scenario("Worldwide Travel")
-    .feed(travel_story_feed)
-    .feed(travel_tagpage_feed)
+    .feed(travelStoryFeed)
+    .exec(http("travel story")
+    .get("${travelStoryUrl}")
+    .check(status.is(200)))
 
-    .randomSwitch(
-      25d -> exec(http("travel story")
-        .get("${travel_story_urls")
-        .check(status.is(200)))
+    .exec(http("travel homepage")
+    .get("/travel")
+    .check(status.is(200)))
 
+    .feed(travelTagPageFeed)
+    .exec(http("travel tag pages")
+    .get("${travelTagpageUrl}")
+    .check(status.is(200)))
 
-
-  25d -> group("HomePage") {
-    exec(http("travel homepage")
-      .get("/travel")
-      .check(status.is(200)))
-
-  }
-
-  25d -> group("Tag Page") {
-    exec(http("travel tag pages")
-      .get("${travel_tagpage_urls}")
-      .check(status.is(200)))
-
-  }
-
-  25d -> group("Destinations Index Page") {
-    exec(http("destinations index page")
-      .get("/travel/destinations")
-      .check(status.is(200)))
-
-  }
-
-  )
-
+    .exec(http("destinations index page")
+    .get("/travel/destinations")
+    .check(status.is(200)))
 
   setUp(scn.inject(
     rampUsersPerSec(4) to(8) during(2 minutes),
