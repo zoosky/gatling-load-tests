66bd28f3f9dcbd0e0863dc9d989e7ed51ad281cb
AdrianLewis <adrian_lewis@fastmail.fm>
2015-02-12 14:49:16 +0000

    Love button test

---
+++ src/test/scala/bbc/lovebutton/LoveButton.scala
@@ -0,0 +1,30 @@
+package bbc.lovebutton
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class LoveButton extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://www.stage.bbc.co.uk")
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("en-GB,en;q=0.5")
+		.connection("keep-alive")
+		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:35.0) Gecko/20100101 Firefox/35.0")
+    .disableCaching
+
+	val scn = scenario("Love Button")
+    .exec(http("Love Button Page")
+    .get("/modules/love/test/load").check(status.is(200))
+    .resources(
+      http("love.css").get("http://static.test.bbci.co.uk/modules/love/0.1.3/style/love.css").check(status.is(200)),
+      http("gelicons-social.ttf").get("http://static.test.bbci.co.uk/gelstyles/0.9.0/fonts/gelicons-social.ttf").check(status.is(200)),
+      http("bbclove.js").get("http://static.test.bbci.co.uk/modules/love/0.1.3/modules/bbclove.js").check(status.is(200))))
+
+  setUp(scn.inject(
+    rampUsersPerSec(10) to(300) during(30 minutes)
+  ).protocols(httpProtocol))
+}
