411a50d49a3e8d266b6c96f8dc9d5dfea386283c
AidyLewis <adrian_lewis@fastmail.fm>
2015-02-24 16:48:10 +0000

    moving back to original injection

--- src/test/scala/bbc/memcache/Memcache.scala
+++ src/test/scala/bbc/memcache/Memcache.scala
@@ -58,8 +58,8 @@ class Memcache extends Simulation {
       }
 
     setUp(
-      hp.inject(constantUsersPerSec(200) during(20 seconds)),
-      food.inject(constantUsersPerSec(50) during(20 seconds)),
-      bbcId.inject(constantUsersPerSec(50) during(20 seconds))
+      hp.inject(constantUsersPerSec(50) during(20 minutes)),
+      food.inject(constantUsersPerSec(10) during(20 minutes)),
+      bbcId.inject(constantUsersPerSec(10) during(20 minutes))
     ).protocols(httpProtocol)
 }
