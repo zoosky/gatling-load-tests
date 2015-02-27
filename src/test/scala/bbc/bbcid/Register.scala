485cc81c6d49cd694028d489ed5d16c070dbfb8b
AidyLewis <adrian_lewis@fastmail.fm>
2015-02-27 15:26:56 +0000

    register test

--- src/test/scala/bbc/bbcid/Register.scala
+++ src/test/scala/bbc/bbcid/Register.scala
@@ -1,45 +1,32 @@
-package bbc.bbcid
+package bbcid
 
 import scala.concurrent.duration._
-
+import scala.util.Random
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 class Register extends Simulation {
 
-  val httpProtocol = http
-    .baseURL("https://ssl.stage.bbc.co.uk")
-    .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-GB,en-US;q=0.8,en;q=0.6")
-    .connection("keep-alive")
-    .contentTypeHeader("application/x-www-form-urlencoded")
-    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36")
+	val httpProtocol = http
+		.baseURL("https://ssl.bbc.co.uk")
+		.inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("en-GB,en;q=0.5")
+		.connection("keep-alive")
+		.contentTypeHeader("application/x-www-form-urlencoded")
+		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:35.0) Gecko/20100101 Firefox/35.0")
 
-    val getHeader = Map("Accept-Encoding" -> "gzip, deflate, sdch")
-    val postHeader = Map("Origin" -> "https://ssl.stage.bbc.co.uk")
+	val scn = scenario("Register")
+    .exec(http("register")
+    .get("/id/register"))
+		.exec(http("post register")
+    .post("/id/register")
+    .formParam("email", session => s"aidy@${new Random().nextInt(100000)}.com")
+    .formParam("confirmpassword", "loadtest")
+    .formParam("confirmpassword_confirm", "loadtest")
+    .check(substring("registration is complete"))
+  )
 
-    // printf '%s\n' ADRIAN@LOADTEST{1..10}.com 
-    val emailFeeder = csv("bbcid/email-addresses.csv").queue 
-
-    val scn = scenario("Register")
-      .feed(emailFeeder)
-      .exec(http("get")
-      .get("/id/register?mode=embedded")
-      .check(status.is(200)) 
-      .headers(getHeader))
-      .exec(http("post")
-      .post("/id/register")
-      .headers(postHeader)
-      .formParam("email", "${email}")
-      .formParam("confirmpassword", "loadtest")
-      .formParam("confirmpassword_confirm", "loadtest")
-      .formParam("bbcid_submit_button", "Register")
-      .check(status.is(200)) 
-      .check(substring("Your registration is complete")))
-
-    setUp(scn.inject(
-      constantUsersPerSec(100) during(45 minutes)
-    ).protocols(httpProtocol))
+	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
