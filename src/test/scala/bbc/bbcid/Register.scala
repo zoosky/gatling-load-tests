02131daf86de5d9ada5852b30896417d5e45304c
AidyLewis <adrian_lewis@fastmail.fm>
2015-03-02 12:23:18 +0000

    new register test

--- src/test/scala/bbc/bbcid/Register.scala
+++ src/test/scala/bbc/bbcid/Register.scala
@@ -8,7 +8,7 @@ import io.gatling.http.Predef._
 class Register extends Simulation {
 
   val httpProtocol = http
-    .baseURL("https://ssl.bbc.co.uk")
+    .baseURL("https://ssl.stage.bbc.co.uk")
     .inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.(t|o)tf""", """.*\.png"""), WhiteList())
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .acceptEncodingHeader("gzip, deflate")
@@ -18,13 +18,29 @@ class Register extends Simulation {
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:35.0) Gecko/20100101 Firefox/35.0")
 
   val scn = scenario("Register")
-    .exec(http("register")
-    .get("/id/register"))
+    .exec(http("get register")
+    .get("/oauth/authorize?response_type=token&client_id=iplayer-ios&redirect_uri=http://www.bbc.co.uk/iPlayer&scope=plays.any.r&action=register"))
+
+    .exec(http("post is old enough")
+    .post("/id/register?ptrt=https%3A%2F%2Fssl.stage.bbc.co.uk%2Foauth%2Fauthorize%3Fredirect_uri%3Dhttp%253A%252F%252Fwww.bbc.co.uk%252FiPlayer%26scope%3Dplays.any.r%26response_type%3Dtoken%26client_id%3Diplayer-ios&mode=embedded")
+    .formParam("isageold", "1")
+    .formParam("uncollected_isoversixteenundersixteen", "1")
+    .formParam("uncollected_username", "")
+    .formParam("uncollected_email", "")
+    .formParam("uncollected_confirmpassword", "")
+    .formParam("uncollected_confirmpassword_confirm", "")
+    .formParam("uncollected_dateofbirthundersixteen_day", "")
+    .formParam("uncollected_dateofbirthundersixteen_month", "")
+    .formParam("uncollected_dateofbirthundersixteen_year", "")
+    .formParam("bbcid_submit_button", "Register"))
+
     .exec(http("post register")
-    .post("/id/register")
-    .formParam("email", session => s"aidy@${new Random().nextInt(2147483647)}.com")
+    .post("/id/register?ptrt=https%3A%2F%2Fssl.stage.bbc.co.uk%2Foauth%2Fauthorize%3Fredirect_uri%3Dhttp%253A%252F%252Fwww.bbc.co.uk%252FiPlayer%26scope%3Dplays.any.r%26response_type%3Dtoken%26client_id%3Diplayer-ios&mode=embedded")
+    .formParam("collected_isageold", "1")
+    .formParam("email", session => s"aidy@${new Random().nextInt(Int.MaxValue)}.com")
     .formParam("confirmpassword", "loadtest")
     .formParam("confirmpassword_confirm", "loadtest")
+    .formParam("bbcid_submit_button", "Register")
     .check(substring("registration is complete")))
 
   setUp(scn.inject(
