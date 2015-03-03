18179d5b7a4fbb9c24cd177f4db3558027fd63f8
AidyLewis <adrian_lewis@fastmail.fm>
2015-03-03 15:11:40 +0000

    travel news filter test

---
+++ src/test/scala/bbc/locator/TravelNewsFilter.scala
@@ -0,0 +1,24 @@
+package bbc.locator
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class TravelNewsFilter extends Simulation {
+
+  val httpProtocol = http.baseURL("https://api.stage.bbc.co.uk")
+
+  val geonameId = csv("locator/ds_001-geonames_domestic-2304.csv").circular 
+
+  val scn = scenario("Travel News Filter")
+    .feed(geonameId)
+    .exec(http("travel news filter")
+    .get("/locator/locations/${geonameId}/details?df=travelnews")
+    .check(status.is(200)))
+
+  setUp(scn.inject(
+    rampUsersPerSec(10) to(300) during(5 minutes),
+    constantUsersPerSec(300) during(15 minutes)
+  ).protocols(httpProtocol))
+}
