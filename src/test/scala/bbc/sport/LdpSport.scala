c0838550e507866a2e90af514bb6ccff13c2447f
AidyLewis <adrian_lewis@fastmail.fm>
2015-03-04 14:44:25 +0000

    more readable format

--- src/test/scala/bbc/sport/LdpSport.scala
+++ src/test/scala/bbc/sport/LdpSport.scala
@@ -1,49 +1,43 @@
-package bbc.sport
+package bbc.loadtest.sport
 
 import scala.concurrent.duration._
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-trait SystemProps {
-  val env = System.getProperty("env")
-}
+import bbc.loadtest.utils.SystemProps
 
 class LdpSport extends Simulation with SystemProps {
+
+  val apiKey = "aszYdyTIisgk9XEwAg9rlnSrjAlDhkWG"
+  val palUrls = csv("sport/pal-client.txt").circular 
+  val sportDataUrls = csv("sport/sport-data.txt").circular 
 
   val httpProtocol = http
     .baseURL(s"https://api.$env.bbc.co.uk/ldp-sport")
     .acceptHeader("application/json-ld")
     .disableCaching
     
-  val apiKey = "aszYdyTIisgk9XEwAg9rlnSrjAlDhkWG"
-
-  val sportPalClient = scenario("Sport PAL client")
-    .group("Football") {
-      exec(http("digests").get(s"/football/competitions/premier-league/teams/digests?api_key=$apiKey").check(status.is(200)))
-      .exec(http("premier league teams").get(s"/football/competitions/premier-league/teams?api_key=$apiKey").check(status.is(200)))
-      .exec(http("sports data").get(s"/football/teams/urn:sports-data:TFBB60?api_key=$apiKey").check(status.is(200)))
-      .exec(http("man united").get(s"/football/team/manchester-united?api_key=$apiKey").check(status.is(200)))
-      .exec(http("venues").get(s"/football/venues?api_key=$apiKey").check(status.is(200)))
-      .exec(http("teams").get(s"/football/teams?api_key=$apiKey").check(status.is(200)))
-      .exec(http("competitions").get(s"/football/competitions/premier-league,championship?api_key=$apiKey").check(status.is(200)))
-      .exec(http("teams paginated").get(s"/football/teams/paginated?pageSize=10&page=1&api_key=$apiKey").check(status.is(200)))
-    }
-
-    .exec(http("boxing").get(s"/boxing/competition/commonwealth-games/stage/mens-heavy-91kg?season=2014&api_key=$apiKey").check(status.is(200)))
-    .exec(http("thing").get(s"/thing/premier-league?api_key=$apiKey").check(status.is(200)))
-    .exec(http("formula1").get(s"/formula1/people?api_key=$apiKey").check(status.is(200)))
-    .exec(http("discipline").get(s"/discipline/football?api_key=$apiKey").check(status.is(200)))
-    .exec(http("winter-olympics").get(s"/winter-olympics/disciplines?api_key=$apiKey").check(status.is(200)))
+  val palClient = scenario("PAL client")
+    .feed(palUrls) 
+    .exec(http("pal client")
+    .get("${palUrl}api_key=" + apiKey)
+    .check(status.is(200)))
 
   val cpsNavbuilder = scenario("CPS Navbuilder")
-    .exec(http("nav builder").get(s"/nav?api_key=$apiKey").check(status.is(200)))
+    .exec(http("nav builder")
+    .get(s"/nav?api_key=$apiKey")
+    .check(status.is(200)))
 
-  // sports data scenario goes here  
-
+  val sportsData = scenario("Sports Data")
+    .feed(sportDataUrls) 
+    .exec(http("sports data")
+    .get("${palUrl}api_key=" + apiKey)
+    .check(status.is(200)))
+    
   setUp(
-    sportPalClient.inject(
-      atOnceUsers(1)
+    palClient.inject(
+      atOnceUsers(11)
     ),
     cpsNavbuilder.inject(
       atOnceUsers(1)
