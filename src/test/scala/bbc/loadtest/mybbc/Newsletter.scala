f9ce1975d1346ee86543624a8b68ffa02c3b804d
AidyLewis <adrian_lewis@fastmail.fm>
2015-03-12 12:26:23 +0000

    I should be able re-use values

--- src/test/scala/bbc/loadtest/mybbc/Newsletter.scala
+++ src/test/scala/bbc/loadtest/mybbc/Newsletter.scala
@@ -15,23 +15,22 @@ class Newsletter extends Simulation {
     .connection("keep-alive")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:35.0) Gecko/20100101 Firefox/35.0")
 
-  val signInSubscribe = scenario("Sign In Subscribe")
-    .exec(http("Newsletter")
+  val newsletterPage = http("newsletterPage")
     .get("/newsletters/thenolanshow")
-    .check(status.is(200)))
+    .check(status.is(200))
 
-    .exec(http("Sign In")
+  val signInGet = http("signInGet") 
     .get("/id/signin?ptrt=https%3A%2F%2Fssl.stage.bbc.co.uk%2Fnewsletters%2Fthenolanshow")
-    .check(status.is(200)))
+    .check(status.is(200))
 
-    .exec(http("Sign In Post")
+  val signInPost = http("signInPost") 
     .post("/id/signin?ptrt=https%3A%2F%2Fssl.stage.bbc.co.uk%2Fnewsletters%2Fthenolanshow")
     .formParam("unique", "adrian@loadtest1.com") 
     .formParam("password", "loadtest")
     .formParam("bbcid_submit_button", "Sign in")
-    .check(substring("Please confirm your age")))
+    .check(substring("Please confirm your age"))
 
-    .exec(http("Subscribe")
+  val subscribe = http("subscribe")
     .post("https://bbcsignups.external.bbc.co.uk/inxmail4/subscription/servlet")
     .formParam("INXMAIL_HTTP_REDIRECT", "http://www.stage.bbc.co.uk/newsletters/thenolanshow/subscribe?ptrt=http://www.stage.bbc.co.uk")
     .formParam("INXMAIL_HTTP_REDIRECT_ERROR", "http://www.stage.bbc.co.uk/newsletters/thenolanshow/error?ptrt=http://www.stage.bbc.co.uk")
@@ -39,7 +38,13 @@ class Newsletter extends Simulation {
     .formParam("tandc_06", "true")
     .formParam("email", "adrian@loadtest1.com")
     .formParam("u13-confirmation", "0")
-    .check(substring("Check your inbox to confirm your email")))
+    .check(substring("Check your inbox to confirm your email"))
+
+  val signInSubscribe = scenario("signInSubscribe")
+    .exec(newsletterPage)
+    .exec(signInGet)
+    .exec(signInPost)
+    .exec(subscribe)
     
   setUp(signInSubscribe.inject(
     atOnceUsers(2)
