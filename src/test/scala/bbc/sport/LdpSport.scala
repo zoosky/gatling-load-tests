2f1c98b2a6e6578ef70033ace63eb90583a91448
AidyLewis <adrian_lewis@fastmail.fm>
2015-03-24 14:33:21 +0000

    removed comments and apiKey

--- src/test/scala/bbc/sport/LdpSport.scala
+++ src/test/scala/bbc/sport/LdpSport.scala
@@ -8,8 +8,7 @@ import bbc.utils._
 class LdpSport extends Simulation {
 
   val palUrls = csv("sport/pal-client.txt").random 
-  val sportDataUrls = csv("sport/sport-data.txt").random
-  val apiValue = "mCJTtc2wG3WKklB7GMrkWi6Fn8bvTPPa" 
+  val apiKey = None
 
   val httpProtocol = http
     .baseURL(BaseUrls.url("data"))
@@ -19,25 +18,12 @@ class LdpSport extends Simulation {
   val ldpSport = scenario("ldpSport")
     .feed(palUrls)
     .exec(http("Pal Client")
-    .get("${palUrl}" + apiValue)
+    .get("${palUrl}" + apiKey)
     .check(status.in(200, 201, 404)))
   
-/*
-    .exec(http("CPS Nav Builder")
-    .get("/nav?api_key=${apiValue}")
-    .check(status.in(200, 201, 404)))
-    
-    .feed(sportDataUrls)
-    .exec(http("Sports Data") 
-    .get("${sportDataUrl}")
-    .header("Accept", "application/xml")
-    .check(status.in(200, 201, 404)))
-*/
   setUp(ldpSport.inject(
     rampUsersPerSec(10) to 200 during(5 minutes),
     constantUsersPerSec(200) during(20 minutes),
-    rampUsersPerSec(200) to 200 during(2 minutes),
-    constantUsersPerSec(200) during(6 minutes),
     rampUsersPerSec(200) to 300 during(2 minutes),
     constantUsersPerSec(300) during(6 minutes),
     rampUsersPerSec(300) to 400 during(2 minutes),
