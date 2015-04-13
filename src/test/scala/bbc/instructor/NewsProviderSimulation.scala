b41616044a03f75e594bbb1e33e5f3a4dabc38b2
PoornimaGunisetti <poornima.gunisetti@bbc.co.uk>
2015-04-13 09:19:16 +0100

    new test added for news redbutton

---
+++ src/test/scala/bbc/instructor/NewsProviderSimulation.scala
@@ -0,0 +1,81 @@
+package instructor
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class NewsProviderSimulation extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://open.stage.bbc.co.uk/redbutton/newsprovider")
+		.acceptHeader("application/json")
+		.connection("keep-alive")
+
+    val random = new scala.util.Random()
+
+    val textIndexChain =
+        exec(
+            http("${category} index")
+                .get("/index/category/${category}/collection.json?platform=html")
+                .check(regex("""(/story\.json\?assetUri=.+?)"""").findAll.saveAs("stories"))
+        )
+        .exec( session => {
+            val stories:List[String] = session("stories").as[List[String]]
+            val storyUrl:String = stories.apply(random.nextInt(stories.size))
+            val pattern = "assetUri=(.+?)&".r
+            val Some(m) = pattern findFirstMatchIn storyUrl
+            val storyId = m.group(1)
+            session.set("storyUrl", storyUrl).set("storyId", storyId)
+            }
+        )
+        .exec(
+            http("text story ${storyId}")
+                .get("${storyUrl}")
+                .check(regex("errorItem").notExists)
+        )
+
+
+
+    val videoIndexChain =
+        exec(
+            http("${category} index")
+                .get("/index/category/${category}/collection.json?platform=html")
+                .check(regex("missingContent").notExists)
+        )
+
+	val scn = scenario("NewsProviderSimulation")
+
+		.exec(http("Default index")
+				.get("/default.json?platform=html"))
+
+		.exec(http("Categories")
+         		.get("/categories.json?platform=html")
+         		.check(regex("""/newsprovider/index/category/(\w+)/""").findAll.saveAs("categories")))
+        .exec( session => {
+            val categories:List[String] = session("categories").as[List[String]]
+            val randomCategory:String = categories.apply(random.nextInt(categories.size))
+            session.set("category", randomCategory)
+            }
+        )
+        .doIfEqualsOrElse("${category}", "videos") {
+            videoIndexChain
+        } {
+            textIndexChain
+        }
+
+
+
+	setUp(scn.inject(
+	    // typical peak usage
+	    constantUsersPerSec(1) during(2 minutes),
+	    nothingFor(30 seconds),
+	    // aspirational peak usage
+	    constantUsersPerSec(7) during(5 minutes),
+        nothingFor(30 seconds),
+        // find breaking point
+	    rampUsersPerSec(7) to(100) during(5 minutes))
+    )
+	.protocols(httpProtocol)
+}
