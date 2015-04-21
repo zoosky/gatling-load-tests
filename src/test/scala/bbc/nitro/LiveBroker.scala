1a6591200cfd8b66b038e050f8c201d4bdbe8457
aidylewis <adrian_lewis@hushmail.com>
2015-04-21 11:28:52 +0100

    nitro test and data

---
+++ src/test/scala/bbc/nitro/LiveBroker.scala
@@ -0,0 +1,25 @@
+package bbc.nitro
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class LiveBroker extends Simulation {
+  
+  val httpProtocol = http
+    .baseURL("https://api.stage.bbc.co.uk")
+    .acceptHeader("application/xml")
+
+  val request = csv("nitro/requests.txt").circular  
+  
+  val scn = scenario("Live Broker")
+    .feed(request)
+    .exec(http("live broker")
+    .get("/live-broker/moments?request=${requestValue}")
+    .check(status.is(200)))
+
+  setUp(scn.inject(
+    constantUsersPerSec(10) during(60 minutes)
+  ).protocols(httpProtocol))
+}
